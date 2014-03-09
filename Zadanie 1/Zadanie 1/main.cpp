9cd8a67d082fe5cb1f55bc09797743e880b6214a
Piechota <piechota33@gmail.com>
2014-03-09 01:17:17 +0100

    bisekcja&naprawa bledu zwiazanego z przyblizeniem

--- Zadanie 1/Zadanie 1/main.cpp
+++ Zadanie 1/Zadanie 1/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-string menu();
+void menu(string &funkcja);
 /*double wielomian(float x);	//funkcja obliczajaca 2x^3 + 4x^2 - 7
 double trygonometria(float x); //funkcja obliczajaca sinx
 double wykladnicza(float x);	//funkcja obliczajaca 5^x
@@ -14,6 +14,7 @@ double wielXwykl(float x);		//funkcja obliczajaca sin (3^x)*/
 
 string funk;		//pelna funkcja
 double wynikFunkcji(double X, string funkcja);	//wartosc funkcji w X
+double bisekcjia(double A, double B, float n, bool iteracje, string &funkcja); //dolna granica przedzialu, gorna granica przedzialu, warunek zatrzymania, czy warunkiem zatrzymania jest ilosc iteracji
 
 int main()
 {
@@ -34,7 +35,7 @@ int main()
 	cout << "Wybrana funkcja dla arg x = 3:" << endl << funkcje[wyb - 1](3) << endl << endl;
 	*/
 
-	funk = menu();
+	menu(funk);
 	system("pause");
 	return 0;
 }
@@ -313,13 +314,52 @@ double wynikFunkcji(double X, string funkcja)
 		
 		found = strFunkcji.find_first_of("+-", 1);
 	}
-	return stod(strFunkcji);
+
+	double rounded = stod(strFunkcji);
+	if (abs(rounded - round(rounded)) < 0.0000009)
+		rounded = round(rounded);
+
+	return rounded;
 }
 
-string menu()
+double bisekcjia(double A, double B, float n, bool iteracje, string &funkcja)
 {
-	/*int wyb;
-	do
+	if (iteracje && n == 0)
+	{
+		cout << endl << "Koniec iteracji na przedziale: [" << A << ", " << B << "]" << endl;
+		return 0;
+	}
+
+	if (wynikFunkcji((A + B) / 2, funkcja) == 0)
+	{
+		cout << endl << "Pierwiastek znaleziony w punkcie: " << (A + B) / 2 << endl;
+		return (A + B) / 2;
+	}
+
+	cout <<"polowa przedzialu: " << wynikFunkcji((A + B) / 2, funkcja) << endl;
+
+	double zero1=0;
+	double zero2=0;
+	if (wynikFunkcji(A, funkcja)*wynikFunkcji((A + B) / 2, funkcja) < 0)
+		zero1 = bisekcjia(A, (A + B) / 2, n - 1, true, funkcja);
+	if (wynikFunkcji(B, funkcja)*wynikFunkcji((A + B) / 2, funkcja) < 0)
+		zero2 = bisekcjia((A + B) / 2, B, n - 1, true, funkcja);
+
+	if (zero1 > 0)
+		return zero1;
+	if (zero2 > 0)
+		return zero2;
+
+	return 0;
+}
+
+void menu(string &funkcja)
+{
+	int wyb;				//sposob obliczania miejsca zerowego
+	char zatrzymanie;		//warunek zatrzymania
+	float n;				//ilosc iteracji lub dokladnosc (zalerzy od warunku zatrzymania)
+	double a, b;			//przedzialy
+	/*do
 	{
 		system("cls");
 		cout << "1) 2x^3 + 4x^2 - 7 \n"
@@ -333,11 +373,46 @@ string menu()
 		cin >> wyb;
 	} while (wyb <1 || wyb >7);
 	return wyb;*/
-	string funkcja;
+	do{
+		system("cls");
 
-	cout << "Wprowadz wzor funkcji:" << endl;
-	getline(cin, funkcja);
-	return funkcja;
+		cout << "Wprowadz wzor funkcji:" << endl;
+		getline(cin, funkcja);
+
+		//zebranie informacji o metodzie liczenia miejsc zerowych
+		cout << endl << "Wybierz metode:" << endl << "0. Wyjscie" << endl << "1. Bisekcjia" << endl;
+		cin >> wyb;
+		cout << endl << "Podaj sposob zatrzymanai:" << endl << "b) Zadana liczba iteracji" << endl;
+		cin >> zatrzymanie;
+		cout << endl << "Podaj przedzial (pamietaj, ze wartosc funkcji na jego krancach musi miec rozne znaki):" << endl << "A: ";
+		cin >> a;
+		cout << endl << "B: ";
+		cin >> b;
+		while (wynikFunkcji(a, funkcja)*wynikFunkcji(b, funkcja) >= 0)
+		{
+			cout << endl << "Znaki przedzialu sa takie same!" << endl;
+			if (wynikFunkcji((a + b) / 2, funkcja) == 0)
+				cout << "ale srodek przedzialu jest miejscem zerowym!" << endl;
+
+			cout << endl << "Podaj przedzial (pamietaj, ze wartosc funkcji na jego krancach musi miec rozne znaki):" << endl << "A: ";
+			cin >> a;
+			cout << endl << "B: ";
+			cin >> b;
+		}
+
+		cout << endl << "Podaj wartosc warunku konczacego:" << endl;
+		cin >> n;
+
+		switch (zatrzymanie)
+		{
+		case 'b':
+			bisekcjia(a, b, n, true, funkcja);
+			break;
+		}
+
+		system("pause");
+
+	} while (wyb != 0);
 }
 /*
 double wielomian(float x)
